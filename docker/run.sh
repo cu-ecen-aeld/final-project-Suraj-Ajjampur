252f17635728b3c6263d6956e985da4eaa2ea328
aamir <aamir@Aamir-VM.myguest.virtualbox.org>
2023-11-19 13:34:11 -0700

    Updated documentation

--- docker/run.sh
+++ docker/run.sh
@@ -3,6 +3,8 @@ pushd $(dirname $0)
 pushd ..
 # Did the run arguments specify a data mount?
 echo $@ | grep ":/data"
+
+# Creating data-default directory when data mount is not specified
 if [ $? -ne 0 ]; then
     echo "No data directory specified, using default at data-default"
     echo "To override location for persistent data dir, pass a volume mount as"
@@ -11,6 +13,10 @@ if [ $? -ne 0 ]; then
     mkdir -p data-default
     datamount="-v $(realpath data-default):/data"
 fi
+
+# Use hosts network stack for the docker container, application will be deployed
+# in NVIDIAs runtime container, use the datamount, and run the container in 
+# interactive mode
 echo "Running docker with arguments ${datamount} $@"
 docker run --net=host --runtime nvidia \
     ${datamount} \
