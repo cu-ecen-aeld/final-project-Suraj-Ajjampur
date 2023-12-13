a5b97f30854b93638cba85d98333206fed736f96
Suraj-Ajjampur <suraj.ajjampur@colorado.edu>
2023-12-13 13:06:17 -0700

    Added coreutils and changed the initscript.sh

--- src/initScript.sh
+++ src/initScript.sh
@@ -13,42 +13,20 @@ cp --no-clobber -r cfg-deepstream-default/* /data/cfg/deepstream/
 mkdir -p /data/cfg/model
 mkdir -p /input
 
-# Define the directory and file name
-input_dir="/data/input"
-input_file="video.mp4"
+# Define input video file path
+input_file="/data/input/video.mp4"
 
-# Create the directory if it doesn't exist
-mkdir -p "$input_dir"
-
-# Check if the default video file exists
-if [ ! -f "${input_dir}/${input_file}" ]; then
-    echo "No default video found at ${input_dir}/${input_file}, starting capture"
-
-    # Start capturing the video with nvgstcapture-1.0
-    nvgstcapture-1.0 --mode=2 --camsrc=0 --cap-dev-node=0 --automate --capture-auto --file-name "${input_dir}/${input_file}" &
-
-    # Wait for the capture process to complete
+# Check for the input video file
+if [ ! -f "$input_file" ]; then
+    echo "No default video found, starting capture"
+    nvgstcapture-1.0 --mode=2 --camsrc=0 --cap-dev-node=0 --automate --capture-auto --file-name "$input_file"
     wait $!
-
-    # Find the captured file with the pattern and rename it
-    captured_file=$(ls ${input_dir}/${input_file}* | head -n 1)
-    if [ -f "$captured_file" ]; then
-        echo "Captured file found: $captured_file"
-        # Only rename the file if it does not already have the correct name
-        if [ "$captured_file" != "${input_dir}/${input_file}" ]; then
-            mv "$captured_file" "${input_dir}/${input_file}"
-        fi
-    else
-        echo "The captured file was not found."
-    fi
-else
-    echo "Default video already exists at ${input_dir}/${input_file}"
+    captured_file=$(ls ${input_file}_* | head -n 1)
+    [ -f "$captured_file" ] && mv "$captured_file" "$input_file"
 fi
 
-# Create a symlink to the input video if it doesn't already exist
-if [ ! -L "/input/${input_file}" ]; then
-    ln -s "${input_dir}/${input_file}" "/input/${input_file}"
-fi
+# Create a symlink to the input video
+ln -sf "$input_file" /input/video.mp4
 
 if [ ! -d /output ]; then
     echo "No dedicated output mount found, using /data/output dir"
